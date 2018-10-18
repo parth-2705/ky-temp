4bac7126908bd1e624a6a0a28d86d2c5e2482694
parth-2705 <40501175+parth-2705@users.noreply.github.com>
2018-10-18 22:53:00 +0530

    Add files via upload

---
+++ lp/js/index.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function() {
+    
+    //activate wow.js
+     new WOW().init();
+  
+    //activate fullpage.js
+    $('body').fullpage({
+      scrollBar: true,
+      navigation: true,
+      sectionSelector: '.panel'
+    });
+  
+
+  
+});
